aeb288ffd915ccf75fe20f559db349d858d1e273
foreveryennefer <whispersinthesoundofsilence2@gmail.com>
2018-09-09 15:14:17 +0100

    Adding basic double-jump

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,8 +2,21 @@ extends KinematicBody2D
 
 var motion = Vector2()
 const SPEED = 750
+const GRAVITY = 1000
+const UP = Vector2(0, -1)
+const JUMP_SPEED = -520
+var DOUBLE_JUMP_COUNTER
 
 func _physics_process(delta):
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
+	
+func _ready():
+	DOUBLE_JUMP_COUNTER = 0
+	
+func run():
 	$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
@@ -15,9 +28,19 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 		$AnimatedSprite.play("idle")
-	move_and_slide(motion)
-		
-		
 
-#func _ready():
-#	pass
+func fall(delta):
+	if is_on_floor():
+		DOUBLE_JUMP_COUNTER = 0
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta
+
+func jump():
+	if Input.is_action_just_pressed("ui_up"):
+		if DOUBLE_JUMP_COUNTER == 0:
+			motion.y = JUMP_SPEED
+			DOUBLE_JUMP_COUNTER = 1
+		elif DOUBLE_JUMP_COUNTER == 1:
+			motion.y = JUMP_SPEED
+			DOUBLE_JUMP_COUNTER = 2
