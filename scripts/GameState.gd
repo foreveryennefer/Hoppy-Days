54fdbb9c0fed2c85e00e4f54f2c361474c7819c6
foreveryennefer <whispersinthesoundofsilence2@gmail.com>
2018-09-11 12:07:46 +0100

    Initial lives counter in GUI working

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -6,12 +6,17 @@ var lives
 func _ready():
 	Global.GameState = self
 	lives = starting_lives
+	update_GUI()
+
+func update_GUI():
+	Global.GUI.update_GUI(lives)
 
 func end_game():
 	get_tree().change_scene("res://Scenes/GameOver.tscn")
 	
 func hurt():
 	lives -= 1
+	update_GUI()
 	Global.Player.hurt()
 	if lives < 0:
 		end_game()
